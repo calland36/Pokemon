115dc60fcd07d15f68af91b70e49c437cd658db5
calland36 <callandglass@gmail.com>
2023-01-06 09:03:01 -0800

    Figured out how to handle weaknesses.

--- src/app/components/pokemon-weakness/pokemon-weakness.component.ts
+++ src/app/components/pokemon-weakness/pokemon-weakness.component.ts
@@ -56,31 +56,48 @@ export class PokemonWeaknessComponent implements OnInit {
   DarkValue: number=1 ;
   count: number=1 ;
 
+  pokeType1!: string | " ";
+  pokeType2!: string | " ";
+
   constructor() { }
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+
+    this.getApi();
+    this.setTypess();
+  }
 
   getApi(){
     
-    // if(this.apiTypeArray){
-    //   console.log(this.apiTypeArray);
-    // this.apiTypeArray.forEach(apiType => this.setApiType(apiType.type.name));
-    // }
+    if(this.apiTypeArray){
+      console.log("this is apitype= "+this.apiTypeArray);
+    //this.apiTypeArray.forEach(apiType => this.setApiType(apiType.type.name));
+    }
     if(this.apiTypeArray[0]){
-      this.setApiType(this.apiTypeArray[0].type.name);
+      this.pokeType1 = this.apiTypeArray[0].type.name;
+     // console.log("this is apitype[0]= "+this.apiTypeArray[0].type.name);
+
+      //this.setApiType(this.apiTypeArray[0].type.name);
     }
     if(this.apiTypeArray[1]){
-      this.singleCallType = false;
-       console.log("this is 1 " + this.apiTypeArray[1].type.name);
-
-      this.setApiType(this.apiTypeArray[1].type.name);
+      // console.log("this is 1 " + this.apiTypeArray[1].type.name);
+       this.pokeType2 = this.apiTypeArray[1].type.name;
+      //this.setApiType(this.apiTypeArray[1].type.name);
+      //this.singleCallType = false;
     }
+    else{
+      this.pokeType2 = " ";
+    }
+
+    console.log("this is pokeType1 " + this.pokeType1);
+    console.log("this is pokeType2 " + this.pokeType2);
+    this.setApiType();
   }
 
   
 
   setTypess(){
-    if (this.singleCallType){
+  //  if (this.singleCallType){
       //console.log("Hit set Weakness 1 time");
     //console.log("Hit set type 1 time = "+ this.pokimanx.name);
 
@@ -138,195 +155,208 @@ export class PokemonWeaknessComponent implements OnInit {
   if(this.DarkValue>= 1.5 ){ 
     this.Dark=true;
   } 
-}
-this.singleCallType = false;
+//}
+//this.singleCallType = false;
   }
 
-  setApiType( ptype: any){
+  setApiType(){
     
       if (this.singleCall){
-        console.log("Ptype= " +ptype);
   
-      if(ptype.includes('fire')){
-        console.log("ptype includes fire [rock start= "+ this.RockValue);
-        this.FireValue= this.FireValue * 0.5;
-        this.WaterValue=this.WaterValue * 2;
-        this.GrassValue= this.GrassValue * 0.5;
-        this.IceValue = this.IceValue * 0.5;
-        this.RockValue= this.RockValue * 2;
-        this.BugValue= this.BugValue * 0.5;
-        this.GroundValue= this.GroundValue * 2;
-        this.SteelValue= this.SteelValue * 0.5;
-        this.FairyValue= this.FairyValue * 0.5;
-        console.log("ptype includes fire [rock end= "+ this.RockValue);
-      }
-      if(ptype.includes('water')){
-        this.FireValue= this.FireValue * 0.5;
-        this.WaterValue= this.WaterValue * 0.5;
-        this.ElectricValue= this.ElectricValue * 2;
-        this.GrassValue= this.GrassValue * 2;
-        this.IceValue= this.IceValue * 0.5;
-        this.SteelValue= this.SteelValue * 0.5;
-  
-      }
-      if(ptype.includes('flying')){
-        console.log("ptype includes FLYING [rock start= "+ this.RockValue);
-        this.ElectricValue= this.ElectricValue * 2;
-        this.GrassValue= this.GrassValue * 0.5;
-        this.IceValue= this.IceValue * 2;
-        this.FightingValue= this.FightingValue * 1.5;
-        this.GroundValue= 0;
-        this.BugValue= this.BugValue * 0.5;
-        this.RockValue= this.RockValue * 2;
-        console.log("ptype includes FLYING [rock end= "+ this.RockValue);
-      }
-      if(ptype.includes('grass')){
-        this.FireValue= this.FireValue * 2;
-        this.WaterValue= this.WaterValue * 0.5;
-        this.ElectricValue= this.ElectricValue * 0.5;
-        this.GrassValue= this.GrassValue * 0.5;
-        this.IceValue= this.IceValue * 2;
-        this.PoisonValue= this.PoisonValue * 2;
-        this.GroundValue= this.GroundValue * 0.5;
+        if(this.pokeType1.includes('fire') || this.pokeType2.includes('fire')){
+          console.log("this.pokeType1 includes fire [rock start= "+ this.RockValue);
+          this.FireValue= this.FireValue * 0.5;
+          this.WaterValue=this.WaterValue * 2;
+          this.GrassValue= this.GrassValue * 0.5;
+          this.IceValue = this.IceValue * 0.5;
+          this.RockValue= this.RockValue * 2;
+          this.BugValue= this.BugValue * 0.5;
+          this.GroundValue= this.GroundValue * 2;
+          this.SteelValue= this.SteelValue * 0.5;
+          this.FairyValue= this.FairyValue * 0.5;
+          console.log("this.pokeType1 includes fire [rock end= "+ this.RockValue);
+        }
+        if(this.pokeType1.includes('water') || this.pokeType2.includes('water')){
+          this.FireValue= this.FireValue * 0.5;
+          this.WaterValue= this.WaterValue * 0.5;
+          this.ElectricValue= this.ElectricValue * 2;
+          this.GrassValue= this.GrassValue * 2;
+          this.IceValue= this.IceValue * 0.5;
+          this.SteelValue= this.SteelValue * 0.5;
+    
+        }
+        if(this.pokeType1.includes('flying') || this.pokeType2.includes('flying')){
+          console.log("this.pokeType1 includes FLYING [rock start= "+ this.RockValue);
+          this.ElectricValue= this.ElectricValue * 2;
+          this.GrassValue= this.GrassValue * 0.5;
+          this.IceValue= this.IceValue * 2;
+          this.FightingValue= this.FightingValue * 1.5;
+          this.GroundValue= 0;
+          this.BugValue= this.BugValue * 0.5;
+          this.RockValue= this.RockValue * 2;
+          console.log("this.pokeType1 includes FLYING [rock end= "+ this.RockValue);
+        }
+        if(this.pokeType1.includes('grass') || this.pokeType2.includes('grass')){
+          this.FireValue= this.FireValue * 2;
+          this.WaterValue= this.WaterValue * 0.5;
+          this.ElectricValue= this.ElectricValue * 0.5;
+          this.GrassValue= this.GrassValue * 0.5;
+          this.IceValue= this.IceValue * 2;
+          this.PoisonValue= this.PoisonValue * 2;
+          this.GroundValue= this.GroundValue * 0.5;
+          this.FlyingValue= this.FlyingValue * 2;
+    
+        }
+        if(this.pokeType1.includes('ground') || this.pokeType2.includes('ground')){
+          this.WaterValue= this.WaterValue * 2;
+          this.ElectricValue= this.ElectricValue * 0;
+          this.GrassValue= this.GrassValue * 2;
+          this.IceValue= this.IceValue * 2;
+          this.PoisonValue= this.PoisonValue *0.5;
+          this.RockValue= this.RockValue * 0.5;
+    
+        }
+        if(this.pokeType1.includes('poison') || this.pokeType2.includes('poison')){
+          console.log("this.pokeType2 includes Poison [rock end= "+ this.RockValue);
+
+          this.GrassValue= this.GrassValue * 0.5;
+          this.PoisonValue= this.PoisonValue * 0.5;
+          this.FightingValue= this.FightingValue * 0.5;
+          this.GroundValue= this.GroundValue * 2;
+          this.PsychicValue= this.PsychicValue * 2;
+          this.BugValue= this.BugValue * 0.5;
+          this.RockValue= this.RockValue * 0.5;
+          this.DarkValue= this.DarkValue * 0.5;
+          this.FairyValue= this.FairyValue * 2;
+          
+    
+        }
+        if(this.pokeType1.includes('bug') || this.pokeType2.includes('bug')){
+          this.FireValue= this.FireValue * 2;
+          this.GrassValue= this.GrassValue * 0.5;
+          this.FightingValue= this.FightingValue * 0.5;
+          this.GroundValue= this.GroundValue * 0.5;
+          this.FlyingValue= this.FlyingValue * 2;
+          this.RockValue= this.RockValue * 2;
+    
+        }
+        if(this.pokeType1.includes('normal') || this.pokeType2.includes('normal')){
+          this.FightingValue= this.FightingValue * 2;
+          this.GhostValue= 0;
+    
+        }
+        if(this.pokeType1.includes('fairy') || this.pokeType2.includes('fairy')){
+          this.FightingValue= this.FightingValue * 0.5;
+          this.PoisonValue= this.PoisonValue * 2;
+          this.BugValue= this.BugValue * 0.5;
+          this.DragonValue= this.DragonValue * 0;
+          this.DarkValue= this.DarkValue * 0.5;
+          this.SteelValue= this.SteelValue * 2;
+    
+        }
+        if(this.pokeType1.includes('fighting') || this.pokeType2.includes('fighting')){
         this.FlyingValue= this.FlyingValue * 2;
-  
-      }
-      if(ptype.includes('ground')){
-        this.WaterValue= this.WaterValue * 2;
-        this.ElectricValue= this.ElectricValue * 0;
-        this.GrassValue= this.GrassValue * 2;
-        this.IceValue= this.IceValue * 2;
-        this.PoisonValue= this.PoisonValue *0.5;
-        this.RockValue= this.RockValue * 0.5;
-  
-      }
-      if(ptype.includes('poison')){
-        this.GrassValue= this.GrassValue * 0.5;
-        this.PoisonValue= this.PoisonValue * 0.5;
-        this.FightingValue= this.FightingValue * 0.5;
-        this.GroundValue= this.GroundValue * 2;
         this.PsychicValue= this.PsychicValue * 2;
         this.BugValue= this.BugValue * 0.5;
         this.RockValue= this.RockValue * 0.5;
         this.DarkValue= this.DarkValue * 0.5;
         this.FairyValue= this.FairyValue * 2;
-        
-  
-      }
-      if(ptype.includes('bug')){
-        this.FireValue= this.FireValue * 2;
-        this.GrassValue= this.GrassValue * 0.5;
-        this.FightingValue= this.FightingValue * 0.5;
-        this.GroundValue= this.GroundValue * 0.5;
-        this.FlyingValue= this.FlyingValue * 2;
-        this.RockValue= this.RockValue * 2;
-  
-      }
-      if(ptype.includes('normal')){
-        this.FightingValue= this.FightingValue * 2;
-        this.GhostValue= 0;
-  
-      }
-      if(ptype.includes('fairy')){
-        this.FightingValue= this.FightingValue * 0.5;
-        this.PoisonValue= this.PoisonValue * 2;
-        this.BugValue= this.BugValue * 0.5;
-        this.DragonValue= this.DragonValue * 0;
-        this.DarkValue= this.DarkValue * 0.5;
-        this.SteelValue= this.SteelValue * 2;
-  
-      }
-      if(ptype.includes('fighting')){
-      this.FlyingValue= this.FlyingValue * 2;
-      this.PsychicValue= this.PsychicValue * 2;
-      this.BugValue= this.BugValue * 0.5;
-      this.RockValue= this.RockValue * 0.5;
-      this.DarkValue= this.DarkValue * 0.5;
-      this.FairyValue= this.FairyValue * 2;
-  
-      }
-      if(ptype.includes('rock')){
-        this.NormalValue= this.NormalValue * 0.5;
-        this.FireValue= this.FireValue *  0.5;
-        this.WaterValue= this.WaterValue * 2;
-        this.GrassValue= this.GrassValue * 2;
-        this.FightingValue= this.FightingValue * 2;
-        this.PoisonValue= this.PoisonValue * 0.5;
-        this.GroundValue= this.GroundValue * 2;
-        this.FlyingValue= this.FlyingValue * 0.5;
-        this.SteelValue= this.SteelValue * 2;
-  
-      }
-      if(ptype.includes('steel')){
-        this.NormalValue= this.NormalValue * 0.5;
-        this.FireValue= this.FireValue * 2;
-        this.GrassValue= this.GrassValue * 0.5;
-        this.IceValue= this.IceValue * 0.5;
-        this.FightingValue= this.FightingValue * 2;
-        this.PoisonValue= this.PoisonValue * 0;
-        this.GroundValue= this.GroundValue * 2;
-        this.FlyingValue= this.FlyingValue * 0.5;
-        this.PsychicValue= this.PsychicValue * 0.5;
-        this.BugValue= this.BugValue * 0.5;
-        this.RockValue= this.RockValue * 0.5;
-        this.DragonValue= this.DragonValue * 0.5;
-        this.SteelValue= this.SteelValue * 0.5;
-        this.FairyValue= this.FairyValue * 0.5;
-  
-      }
-      if(ptype.includes('ice')){
-        this.FireValue= this.FireValue * 2;
-        this.IceValue= this.IceValue * 0.5;
-        this.FightingValue= this.FightingValue * 2;
-        this.RockValue= this.RockValue * 2;
-        this.SteelValue= this.SteelValue * 2;
-      }
-      if(ptype.includes('ghost')){
-        this.NormalValue= this.NormalValue * 0;
-        this.FightingValue= this.FightingValue * 0;
-        this.PoisonValue= this.PoisonValue * 0.5;
-        this.BugValue= this.BugValue * 0.5;
-        this.GhostValue= this.GhostValue * 2;
-        this.DarkValue= this.DarkValue * 2;
-  
-      }
-      if(ptype.includes('psychic')){
-        this.FightingValue= this.FightingValue * 0.5;
-        this.PsychicValue= this.PsychicValue * 0.5;
-        this.BugValue= this.BugValue * 2;
-        this.GhostValue= this.GhostValue * 2;
-        this.DarkValue= this.DarkValue * 2;
-  
-      }
-      if(ptype.includes('electric')){
-        this.ElectricValue= this.ElectricValue * 0.5;
-        this.GroundValue= this.GroundValue * 2;
-        this.FlyingValue= this.FlyingValue * 0.5;
-        this.SteelValue= this.SteelValue * 0.5;
-  
-      }
-      if(ptype.includes('dragon')){
-        this.FireValue= this.FireValue * 0.5;
-        this.WaterValue= this.WaterValue * 0.5;
-        this.ElectricValue= this.ElectricValue * 0.5;
-        this.GrassValue= this.GrassValue * 0.5;
-        this.IceValue= this.IceValue * 2;
-        this.DragonValue= this.DragonValue * 2;
-        this.FairyValue= this.FairyValue * 2;
-  
-      }
-      if(ptype.includes('dark')){
-        this.FightingValue= this.FightingValue * 2;
-        this.PsychicValue= this.PsychicValue * 0;
-        this.BugValue= this.BugValue * 2;
-        this.GhostValue= this.GhostValue * 0.5;
-        this.DarkValue= this.DarkValue * 0.5;
-        this.FairyValue= this.FairyValue * 2;
-  
-      }
+    
+        }
+        if(this.pokeType1.includes('rock') || this.pokeType2.includes('rock')){
+          this.NormalValue= this.NormalValue * 0.5;
+          this.FireValue= this.FireValue *  0.5;
+          this.WaterValue= this.WaterValue * 2;
+          this.GrassValue= this.GrassValue * 2;
+          this.FightingValue= this.FightingValue * 2;
+          this.PoisonValue= this.PoisonValue * 0.5;
+          this.GroundValue= this.GroundValue * 2;
+          this.FlyingValue= this.FlyingValue * 0.5;
+          this.SteelValue= this.SteelValue * 2;
+    
+        }
+        if(this.pokeType1.includes('steel') || this.pokeType2.includes('steel')){
+          this.NormalValue= this.NormalValue * 0.5;
+          this.FireValue= this.FireValue * 2;
+          this.GrassValue= this.GrassValue * 0.5;
+          this.IceValue= this.IceValue * 0.5;
+          this.FightingValue= this.FightingValue * 2;
+          this.PoisonValue= this.PoisonValue * 0;
+          this.GroundValue= this.GroundValue * 2;
+          this.FlyingValue= this.FlyingValue * 0.5;
+          this.PsychicValue= this.PsychicValue * 0.5;
+          this.BugValue= this.BugValue * 0.5;
+          this.RockValue= this.RockValue * 0.5;
+          this.DragonValue= this.DragonValue * 0.5;
+          this.SteelValue= this.SteelValue * 0.5;
+          this.FairyValue= this.FairyValue * 0.5;
+    
+        }
+        if(this.pokeType1.includes('ice') || this.pokeType2.includes('ice')){
+          this.FireValue= this.FireValue * 2;
+          this.IceValue= this.IceValue * 0.5;
+          this.FightingValue= this.FightingValue * 2;
+          this.RockValue= this.RockValue * 2;
+          this.SteelValue= this.SteelValue * 2;
+        }
+        if(this.pokeType1.includes('ghost') || this.pokeType2.includes('ghost')){
+          this.NormalValue= this.NormalValue * 0;
+          this.FightingValue= this.FightingValue * 0;
+          this.PoisonValue= this.PoisonValue * 0.5;
+          this.BugValue= this.BugValue * 0.5;
+          this.GhostValue= this.GhostValue * 2;
+          this.DarkValue= this.DarkValue * 2;
+    
+        }
+        if(this.pokeType1.includes('psychic') || this.pokeType2.includes('psychic')){
+          this.FightingValue= this.FightingValue * 0.5;
+          this.PsychicValue= this.PsychicValue * 0.5;
+          this.BugValue= this.BugValue * 2;
+          this.GhostValue= this.GhostValue * 2;
+          this.DarkValue= this.DarkValue * 2;
+    
+        }
+        if(this.pokeType1.includes('electric') || this.pokeType2.includes('electric')){
+          this.ElectricValue= this.ElectricValue * 0.5;
+          this.GroundValue= this.GroundValue * 2;
+          this.FlyingValue= this.FlyingValue * 0.5;
+          this.SteelValue= this.SteelValue * 0.5;
+    
+        }
+        if(this.pokeType1.includes('dragon') || this.pokeType2.includes('dragon')){
+          this.FireValue= this.FireValue * 0.5;
+          this.WaterValue= this.WaterValue * 0.5;
+          this.ElectricValue= this.ElectricValue * 0.5;
+          this.GrassValue= this.GrassValue * 0.5;
+          this.IceValue= this.IceValue * 2;
+          this.DragonValue= this.DragonValue * 2;
+          this.FairyValue= this.FairyValue * 2;
+    
+        }
+        if(this.pokeType1.includes('dark') || this.pokeType2.includes('dark')){
+          this.FightingValue= this.FightingValue * 2;
+          this.PsychicValue= this.PsychicValue * 0;
+          this.BugValue= this.BugValue * 2;
+          this.GhostValue= this.GhostValue * 0.5;
+          this.DarkValue= this.DarkValue * 0.5;
+          this.FairyValue= this.FairyValue * 2;
+    
+        }
   
     
     }
+    // if(this.apiTypeArray[0].type.name.includes('grass')){ 
+    //   console.log("Calland check if 0 has grass.");
+    // }
+    // if(this.apiTypeArray[1].type.name.includes('grass')){ 
+    //   console.log("Calland check if 1 has grass.");
+    // }
+    // if(this.apiTypeArray[0].type.name.includes('poison')){ 
+    //   console.log("Calland check if 0 has poison.");
+    // }
+    // if(this.apiTypeArray[1].type.name.includes('poison')){ 
+    //   console.log("Calland check if 1 has poison.");
+    // }
     this.singleCall=false;
     //this.setApiType();
     }
